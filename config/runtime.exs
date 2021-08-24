203f38cb2eb6f1baa9c3daa98f3e980c25edb031
Yiming Chen <dsdshcym@gmail.com>
2021-08-24 17:40:24 +0800

    Revert "init: update config/runtime.exs for fly.io"
    This reverts commit f196949234025761eb6c5af154f02d035b717dff.
    
    - otherwise, =mix assets.deploy= fails due to =FLY_APP_NAME not available=
    - by reverting the commit, we fix the issue by decoupling runtime.exs
      from fly.io (FLY_APP_NAME)

--- config/runtime.exs
+++ config/runtime.exs
@@ -14,17 +14,14 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  app_name =
-    System.get_env("FLY_APP_NAME") ||
-      raise "FLY_APP_NAME not available"
-
   config :csskatas, CSSKatasWeb.Endpoint,
-    server: true,
-    url: [host: "#{app_name}.fly.dev", port: 80],
     http: [
-      port: String.to_integer(System.get_env("PORT") || "4000"),
-      # IMPORTANT: support IPv6 addresses
-      transport_options: [socket_opts: [:inet6]]
+      # Enable IPv6 and bind on all interfaces.
+      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
+      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
+      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
+      ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      port: String.to_integer(System.get_env("PORT") || "4000")
     ],
     secret_key_base: secret_key_base
 
