dbb0a64b3f852024cf6c34bfefffe83a545208c0
Yiming Chen <dsdshcym@gmail.com>
2021-08-29 09:10:49 +0800

    extract CSSKatas.get_kata/1 function
    - prepare to load katas from files in this git repo

--- lib/csskatas.ex
+++ lib/csskatas.ex
@@ -6,4 +6,15 @@ defmodule CSSKatas do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  def get_kata(id) do
+    kata = %{
+      initial_html: ~s{<button class="border rounded">Submit</button>},
+      design: ~s{<button class="border rounded px-4 py-2">Submit</button>},
+      title: ~s{Tailwind CSS 101 - Paddings},
+      instruction: ~s{<p>This is how padding works in Tailwind CSS</p>}
+    }
+
+    {:ok, kata}
+  end
 end
--- lib/csskatas_web/controllers/kata_controller.ex
+++ lib/csskatas_web/controllers/kata_controller.ex
@@ -2,12 +2,7 @@ defmodule CSSKatasWeb.KataController do
   use CSSKatasWeb, :controller
 
   def show(conn, params) do
-    kata = %{
-      initial_html: ~s{<button class="border rounded">Submit</button>},
-      design: ~s{<button class="border rounded px-4 py-2">Submit</button>},
-      title: ~s{Tailwind CSS 101 - Paddings},
-      instruction: ~s{<p>This is how padding works in Tailwind CSS</p>}
-    }
+    {:ok, kata} = CSSKatas.get_kata(params["id"])
 
     conn
     |> render(:show, kata: kata)
