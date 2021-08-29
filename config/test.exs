4dbd1c89f3309616a9574f8120ee5f2c76dc6dfa
Yiming Chen <dsdshcym@gmail.com>
2021-08-29 08:16:24 +0800

    start phoenix server in test environment for feature testing
    - see https://github.com/elixir-wallaby/wallaby#phoenix

--- config/test.exs
+++ config/test.exs
@@ -1,10 +1,8 @@
 import Config
 
-# We don't run a server during test. If one is required,
-# you can enable the server option below.
 config :csskatas, CSSKatasWeb.Endpoint,
   http: [ip: {127, 0, 0, 1}, port: 4002],
-  server: false
+  server: true
 
 # In test we don't send emails.
 config :csskatas, CSSKatas.Mailer,
