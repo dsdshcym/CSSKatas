48635e80348e78dc049adafd9540a3a8374bed47
Yiming Chen <dsdshcym@gmail.com>
2021-08-30 17:44:53 +0800

    add "Inter var" to tailwind's font-sans family
    - see also https://tailwindui.com/documentation#optional-add-the-inter-font-family

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,9 +1,15 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 module.exports = {
   mode: "jit",
   purge: ["./js/**/*.js", "../lib/*_web/**/*.*ex"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},
