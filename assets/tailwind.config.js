902f12d4b89a217fcdc80ee76a62097b9eec5aed
Yiming Chen <dsdshcym@gmail.com>
2021-09-05 17:38:02 +0800

    let jit "purge" md files as well
    - in case that we need to use tailwind classes in md files

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
   mode: "jit",
-  purge: ["./js/**/*.js", "../lib/*_web/**/*.*ex", "../katas/**/*.html"],
+  purge: ["./js/**/*.js", "../lib/*_web/**/*.*ex", "../katas/**/*.html", "../katas/**/*.md"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
