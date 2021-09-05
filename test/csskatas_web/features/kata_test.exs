2d5ebe81bd16bf39813c3d70054efe06074382f7
Yiming Chen <dsdshcym@gmail.com>
2021-09-05 13:28:31 +0800

    use assert_text instead of assert_has css
    - avoid the assumption that it's in a `p` tag

--- test/csskatas_web/features/kata_test.exs
+++ test/csskatas_web/features/kata_test.exs
@@ -11,7 +11,7 @@ defmodule CSSKatasWeb.Features.KataTest do
     |> assert_has(css("h2", text: "Tailwind CSS 101 - Paddings"))
 
     # Reads instruction and initial solution
-    |> assert_has(css("p", text: "To make a button, we need to add some spaces"))
+    |> assert_text("To make a button, we need to add some spaces")
     |> assert_filled_solution(~s{<button class="border rounded">Button</button>})
 
     # Checks solution that is not a match
