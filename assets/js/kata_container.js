459f378eb95a5f2d10dcdca546d98736cffa4292
Yiming Chen <dsdshcym@gmail.com>
2021-09-08 18:44:09 +0800

    pin tailwindcss version to 2.2.10
    - with @^2.0, unpkg is returning a broken version now
    - 2.2.10 is the latest version,
      see [[https://github.com/tailwindlabs/tailwindcss/releases/tag/v2.2.10][Release v2.2.10 · tailwindlabs/tailwindcss]]
    - by pinning this version, we will have a more reproducible build

--- assets/js/kata_container.js
+++ assets/js/kata_container.js
@@ -14,7 +14,7 @@ let preflight = () => {
         super()
 
         const shadowRoot = this.attachShadow({ mode: "open" })
-        shadowRoot.innerHTML = `<link href="https://unpkg.com/tailwindcss@^2.0/dist/tailwind.min.css" rel="stylesheet"> <div id="preview" class="flex justify-center"></div>`
+        shadowRoot.innerHTML = `<link href="https://unpkg.com/tailwindcss@2.2.10/dist/tailwind.min.css" rel="stylesheet"> <div id="preview" class="flex justify-center"></div>`
       }
     }
   )
