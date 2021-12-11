09a94fdfae231807e1c719c2ec333260905eebf2
Yejun Su <yejun@hey.com>
2021-12-11 21:09:45 +0800

    update tailwindcss configuration
    https://tailwindcss.com/docs/upgrade-guide

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,23 +1,18 @@
 const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
-  mode: "jit",
-  purge: [
+  content: [
     "./js/**/*.js",
     "../lib/*_web/**/*.*ex",
     "../tracks/**/*.html",
     "../tracks/**/*.md"
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [
     require('@tailwindcss/typography'),
