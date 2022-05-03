7d624055a92675df4243686771a127f84cec3426
Yejun Su <yejun@hey.com>
2022-05-03 18:04:36 +0800

    slug -> title

--- lib/mix/tasks/kata/new.ex
+++ lib/mix/tasks/kata/new.ex
@@ -8,23 +8,23 @@ defmodule Mix.Tasks.Kata.New do
   So you can change position by running the same command with a new position value.
 
   ## Options
-  - `--slug` - a string joined with hyphen, e.g. "space-between". (required)
+  - `--title` - name of the track, e.g. "rounded avatar". (required)
   - `--track` - a directory name chosed from the tracks directory. (default: "tailwind-css-101")
   - `--position` - a float number, smaller value has a higher order. (default: 0.1)
 
   ## Examples
 
-      $ mix kata.new --slug "space-between"
+      $ mix kata.new --title "rounded avatar"
 
   Is equivalent to:
 
-      $ mix kata.new --slug "space-between" --track "tailwind-css-101" --position=0.1
+      $ mix kata.new --title "rounded avatar" --track "tailwind-css-101" --position=0.1
 
   """
 
   use Mix.Task
 
-  @switches [track: :string, slug: :string, position: :float]
+  @switches [title: :string, track: :string, position: :float]
 
   @katas_home "./tracks"
   @default_position 0.1
@@ -35,26 +35,26 @@ defmodule Mix.Tasks.Kata.New do
   def run(args) do
     {parsed, _, _} = OptionParser.parse(args, strict: @switches)
 
-    slug = Keyword.fetch!(parsed, :slug)
+    title = Keyword.fetch!(parsed, :title)
     position = Keyword.get(parsed, :position, @default_position)
     track = Keyword.get(parsed, :track, @default_track)
 
+    slug = get_slug_from_title(title)
     dir = get_dir_from_track_slug(track, slug)
-    title = get_title_from_slug(slug)
     create_files(dir, position, title)
 
     IO.puts("Please run command `mix compile --force` manually. to compile this new Kata.")
   end
 
-  defp get_dir_from_track_slug(track, slug) do
-    Path.join([@katas_home, track, slug])
+  defp get_slug_from_title(title) do
+    title
+    |> String.downcase()
+    |> String.split()
+    |> Enum.join("-")
   end
 
-  defp get_title_from_slug(slug) do
-    slug
-    |> String.split("-")
-    |> Enum.map(&String.capitalize/1)
-    |> Enum.join(" ")
+  defp get_dir_from_track_slug(track, slug) do
+    Path.join([@katas_home, track, slug])
   end
 
   defp create_files(dir, position, title) do
