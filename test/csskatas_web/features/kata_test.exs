2182096762c949aa56ff578acfabce98c83c46f4
Yejun Su <yejun@hey.com>
2022-05-12 00:03:15 +0800

    test all katas

--- test/csskatas_web/features/kata_test.exs
+++ test/csskatas_web/features/kata_test.exs
@@ -43,6 +43,73 @@ defmodule CSSKatasWeb.Features.KataTest do
     |> assert_has(css("h1", text: "Tracks"))
   end
 
+  describe "test kata by slug" do
+    setup context do
+      slug =
+        context[:test]
+        |> Atom.to_string()
+        |> String.split()
+        |> List.last()
+
+      {:ok, kata} = CSSKatas.get_kata(slug)
+      {:ok, kata: kata}
+    end
+
+    feature("welcome-to-csskatas", context, do: do_test_kata(context))
+    feature("a-utility-first-framework", context, do: do_test_kata(context))
+    feature("text-color", context, do: do_test_kata(context))
+    feature("text-transform", context, do: do_test_kata(context))
+    feature("font-format", context, do: do_test_kata(context))
+    feature("button-with-paddings", context, do: do_test_kata(context))
+    feature("button-with-ring", context, do: do_test_kata(context))
+    feature("button-with-bg-color", context, do: do_test_kata(context))
+    feature("justify-between-two-buttons", context, do: do_test_kata(context))
+    feature("rounded-avatar", context, do: do_test_kata(context))
+    feature("gradient-text", context, do: do_test_kata(context))
+    feature("decorative-quote-mark", context, do: do_test_kata(context))
+  end
+
+  defp do_test_kata(%{session: session, kata: %{slug: "a-utility-first-framework"} = kata}) do
+    session
+    |> visit("/katas/#{kata.slug}")
+    |> assert_has(css("h2", text: kata.title))
+    |> assert_filled_solution(kata.initial_html)
+
+    # Checks initial is just the solution
+    |> click(button("Check"))
+    |> assert_show_congrat_message()
+  end
+
+  defp do_test_kata(%{session: session, kata: kata}) do
+    session
+    |> visit("/katas/#{kata.slug}")
+    |> assert_has(css("h2", text: kata.title))
+    |> assert_filled_solution(kata.initial_html)
+
+    # Checks initial html is not a match
+    |> click(button("Check"))
+    |> assert_error_appeared()
+
+    # Reset the editor
+    |> click(button("Reset"))
+    |> assert_error_dismissed()
+
+    # Checks solution that is not a match
+    |> fill_solution(Regex.replace(~r/[a-z]/, kata.design, "!", global: false))
+    |> click(button("Check"))
+    |> assert_error_appeared()
+
+    # Reset the editor
+    |> click(button("Reset"))
+    |> assert_filled_solution(kata.initial_html)
+    |> assert_error_dismissed()
+
+    # Checks solution that is a match
+    |> fill_solution(kata.design)
+    |> click(button("Check"))
+    |> assert_show_congrat_message()
+  end
+
   defp assert_filled_solution(session, text) do
     session
     |> execute_script("return window.editor_view.state.doc.toString()", [], fn result ->
